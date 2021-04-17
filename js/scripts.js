2f3c91fe7aa8b7a9e96b55a8c7590d9901786972
Adstrat <73385073+Adstrat@users.noreply.github.com>
2021-04-17 08:43:34 +0200

    read more button moved after text

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-document.querySelectorAll(".show-more").forEach(function(button) {
-  button.addEventListener("click", function() {
-    button.nextElementSibling.classList.toggle("isVisible");
+document.querySelectorAll(".show-more").forEach(function (button) {
+  button.addEventListener("click", function () {
+    button.previousElementSibling.classList.toggle("isVisible");
   });
 });
